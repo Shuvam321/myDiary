cb680563edb115c5cdf67e599cc78db851f1a14e
shuvam321 <kshuvam360@gmail.com>
2023-03-03 17:48:30 +0530

    searchBar responsive

--- JS/myDiary.js
+++ JS/myDiary.js
@@ -1,1 +1,11 @@
-
+function searchClick(anchor) {
+    anchor.closest('.wrapper').classList.toggle('active');
+    // console.log("hiii")
+    var search = document.getElementById('search');
+    console.log(search);
+    search.style.display="inline-block";
+    search.style.width="100%"
+    var searchIcon = document.getElementById('search-icon');
+    console.log(searchIcon);
+    searchIcon.style.display="none"
+  }
